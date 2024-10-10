4726a432953b6a1acb39bf737c9819f71798cc8c
Goyal <rishagoyal@deloitte.com>
2024-10-11 01:44:00 +0530

    adding components like header, footer etc

--- BoldPen/src/components/Header/Header.jsx
+++ BoldPen/src/components/Header/Header.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { act } from 'react'
+import {Container, Logo, LogoutBtn} from '../index'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-export default function Header() {
-  return (
-    <div>Header</div>
+function Header() {
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+  const navItems = [
+    {
+      name: 'Home',
+      slug: '/',
+      active: true
+    },
+    {
+      name: 'Login',
+      slug: '/login',
+      active: !authStatus
+    },
+    {
+      name: 'Signup',
+      slug: '/signup',
+      active: !authStatus
+    },
+    {
+      name: 'All Posts',
+      slug: '/all-posts',
+      active: authStatus
+    },
+    {
+      name: 'Add Post',
+      slug: '/add-post',
+      active: authStatus
+    },
+  ]
+  
+  return(
+    <div className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px' />
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item)=>{
+              item.active ? (
+                <li className='mr-3' key={item.name}>
+                  <button onClick={() => navigate(item.slug)}>{item.name}</button>
+                </li>
+              ) : null
+            })}
+            {authStatus && (
+              <li className='mr-3'>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </div>
   )
 }
+
+
+export default Header
