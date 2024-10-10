8262a413e937830aeaa555ed88ffb24f2fe30a05
Goyal <rishagoyal@deloitte.com>
2024-10-11 00:30:51 +0530

    redux setup

--- BoldPen/src/App.jsx
+++ BoldPen/src/App.jsx
@@ -1,13 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import { Header, Footer } from './components'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
 
-  return (
-    <>
-      <h1>BLOG APP</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        Todo<Outlet />
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
