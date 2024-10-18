0ed656a8ba04d2250fd1a222fc4246b91c516eea
Goyal <rishagoyal@deloitte.com>
2024-10-18 23:55:40 +0530

    adding react hook form

---
+++ BoldPen/src/components/AuthLayout.jsx
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+function Protected({children, authentication = true}) {
+    const authStatus = useSelector(state => state.auth.status)
+    const navigate = useNavigate()
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(()=>{
+        if(authentication && authStatus !== authentication){
+            navigate('/login')
+        }else if(!authentication && authStatus === authentication){
+            navigate('/')
+        }
+        setIsLoading(false)
+    },[authStatus, navigate, authentication])
+  return isLoading ? <h1>Loading...</h1> : <>{children}</> 
+}
+
+export default Protected
